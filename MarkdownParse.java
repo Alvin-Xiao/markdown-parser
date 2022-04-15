fe7c45ace5e4b723c7c10cfc60e46427ef95393a
Alvin <alxiao@ucsd.edu>
2022-04-14 19:31:24 -0700

    fixed issue with code mistaking links as images

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,12 +16,12 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if (openBracket - 1 == markdown.indexOf("!", currentIndex)) {
+            if (openBracket - 1 == markdown.indexOf("!", currentIndex) && openBracket != 0) {
                 currentIndex = closeParen + 1;
             }
             else {
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
             }
         }
 
---
+++ test-file3.md
@@ -0,0 +1,2 @@
+[some link](some-link.com)
+[some link 2](some-link.com)
