72490ab199371c8baf46a5a39a7c4d1e58e3d00e
Alvin <alxiao@ucsd.edu>
2022-04-21 19:26:33 -0700

    JUnit checked test-file.md

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,23 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testGetLinks() throws IOException {
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        List<String> expected = List.of("https://something.com", "some-thing.html");
+        assertEquals(expected, links);
     }
 }
