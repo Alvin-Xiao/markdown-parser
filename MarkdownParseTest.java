bb6223f24d9174698f2a32f9d52a424f22a3bbca
Alvin <alxiao@ucsd.edu>
2022-04-21 19:30:12 -0700

    JUnit checked the rest of the test files

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -20,4 +20,31 @@ public class MarkdownParseTest {
         List<String> expected = List.of("https://something.com", "some-thing.html");
         assertEquals(expected, links);
     }
+    
+    @Test
+    public void testGetLinks2() throws IOException {
+        Path fileName = Path.of("test-file2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        List<String> expected = List.of("https:google.com");
+        assertEquals(expected, links);
+    }
+
+    @Test
+    public void testGetLinks3() throws IOException {
+        Path fileName = Path.of("test-file3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        List<String> expected = List.of("some-link.com", "some-link.com");
+        assertEquals(expected, links);
+    }
+
+    @Test
+    public void testGetLinks4() throws IOException {
+        Path fileName = Path.of("test-file4.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        List<String> expected = List.of("some-link.com");
+        assertEquals(expected, links);
+    }
 }
