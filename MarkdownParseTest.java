bec74a671408b1ee4f786c33b3b6c05601b60cc8
Alvin <alxiao@ucsd.edu>
2022-04-28 19:12:22 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -14,36 +14,36 @@ public class MarkdownParseTest {
 
     @Test
     public void testGetLinks() throws IOException {
-        Path fileName = Path.of("test-file.md");
+        Path fileName = Path.of("test-fileA.md");
         String content = Files.readString(fileName);
-        ArrayList<String> links = MarkdownParse.getLinks(content);
+        ArrayList<String> links = MarkdownParse2.getLinks(content);
         List<String> expected = List.of("https://something.com", "some-thing.html");
         assertEquals(expected, links);
     }
     
     @Test
     public void testGetLinks2() throws IOException {
-        Path fileName = Path.of("test-file2.md");
+        Path fileName = Path.of("test-fileA2.md");
         String content = Files.readString(fileName);
-        ArrayList<String> links = MarkdownParse.getLinks(content);
+        ArrayList<String> links = MarkdownParse2.getLinks(content);
         List<String> expected = List.of("https:google.com");
         assertEquals(expected, links);
     }
 
     @Test
     public void testGetLinks3() throws IOException {
-        Path fileName = Path.of("test-file3.md");
+        Path fileName = Path.of("test-fileA3.md");
         String content = Files.readString(fileName);
-        ArrayList<String> links = MarkdownParse.getLinks(content);
+        ArrayList<String> links = MarkdownParse2.getLinks(content);
         List<String> expected = List.of("some-link.com", "some-link.com");
         assertEquals(expected, links);
     }
 
     @Test
     public void testGetLinks4() throws IOException {
-        Path fileName = Path.of("test-file4.md");
+        Path fileName = Path.of("test-fileA4.md");
         String content = Files.readString(fileName);
-        ArrayList<String> links = MarkdownParse.getLinks(content);
+        ArrayList<String> links = MarkdownParse2.getLinks(content);
         List<String> expected = List.of("some-link.com");
         assertEquals(expected, links);
     }
